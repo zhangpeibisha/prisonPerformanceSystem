1b14183cfabd793533c85aa6c7a19eac0f2e38ea
tangdaijia <253141924@qq.com>
2018-03-12 17:17:03 +0800

    全局样式优化

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
         var name = $("#name").val();
         var pass = $("#password").val();
         pass= hex_md5(pass);
+        console.info(pass);
         $.ajax({
             type: 'POST',
             url: registerUrl,
