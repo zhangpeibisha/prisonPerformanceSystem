20e8c8c898f0026ebc99ecd4c1f21b30bc10c9a7
zhangpei <zhangpe0312@qq.com>
2018-03-13 22:29:52 +0800

    修改月季度统计服务类没有加注解的异常

--- src/main/java/org/nix/dao/service/PersonalMonthOvertimeService.java
+++ src/main/java/org/nix/dao/service/PersonalMonthOvertimeService.java
@@ -3,12 +3,14 @@ package org.nix.dao.service;
 import org.apache.log4j.Logger;
 import org.nix.dao.base.SupperBaseDAOImp;
 import org.nix.domain.entity.PersonalMonthOvertime;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Create by zhangpe0312@qq.com on 2018/3/13.
  */
+@Service
 public class PersonalMonthOvertimeService extends SupperBaseDAOImp<PersonalMonthOvertime> {
     //日志记录
     private static Logger logger = Logger.getLogger(PersonalMonthOvertimeService.class);
